627525b6b5bffd9598a8e4100f6d8f6ebc38e95b
olioldfield <151826037+olioldfield@users.noreply.github.com>
2026-06-12 16:04:51 +0100

    Update cors

--- backend/app/main.py
+++ backend/app/main.py
@@ -14,6 +14,7 @@ app.add_middleware(
         "http://127.0.0.1:3000",
         "https://f1-quali-predictor-web.onrender.com",
     ],
+    allow_origin_regex=r"https://.*\.onrender\.com",
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
